37990d54b7a478a75f4f1cb683c2e5312c846c0f
rob <darabandev@gmail.com>
2021-01-22 12:01:05 -0500

    Session routes working

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -32,4 +32,13 @@ router.delete("/", (_req, res) => {
   return res.json({ message: "success" });
 });
 
+router.get("/", restoreUser, (req, res) => {
+  const { user } = req;
+  if (user) {
+    return res.json({ user: user.toSafeObject() });
+  } else {
+    return res.json({});
+  }
+});
+
 module.exports = router;
