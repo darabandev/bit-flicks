b7cc0aadf69e6b96f18e811ececd1b6c8a9e9c39
rob <darabandev@gmail.com>
2021-01-21 19:10:43 -0500

    JWT auth items complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,29 @@
 const router = require("express").Router();
+const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+const asyncHandler = require("express-async-handler");
 
 // router.post("/test", (req, res) => {
 //   res.json({ requestBody: req.body });
+// });
+
+// router.get(
+//   "/set-token-cookie",
+//   asyncHandler(async (req, res) => {
+//     const user = await User.findOne({
+//       where: { username: "Demo-lition" },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   })
+// );
+
+// router.get("/restore-user", restoreUser, (req, res) => {
+//   return res.json(req.user);
+// });
+
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
 // });
 
 module.exports = router;
