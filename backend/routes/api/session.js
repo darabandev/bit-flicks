a387e1f2e16e5a1584bc22483a9756322aa6e337
rob <darabandev@gmail.com>
2021-01-22 13:07:40 -0500

    Signup validators complete

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
 
+const validateLogin = [
+  check("credential").exists({ checkFalsy: true }).notEmpty().withMessage("Please provide a valid email or username."),
+  check("password").exists({ checkFalsy: true }).withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
