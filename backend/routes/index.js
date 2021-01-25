42a05db8eb57a4ea8824e07d19cad763f10e9887
rob <darabandev@gmail.com>
2021-01-25 16:43:33 -0500

    Added heroku code

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -9,26 +9,25 @@ if (process.env.NODE_ENV === "production") {
   // Serve the frontend's index.html file at the root route
   router.get("/", (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
   });
+
   // Serve the static assets in the frontend's build folder
   router.use(express.static(path.resolve("../frontend/build")));
+
   // Serve the frontend's index.html file at all other routes NOT starting with /api
   router.get(/^(?!\/?api).*/, (req, res) => {
     res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
-  });
-}
-if (process.env.NODE_ENV !== "production") {
-  router.get("/api/csrf/restore", (req, res) => {
-    res.cookie("XSRF-TOKEN", req.csrfToken());
-    return res.json({});
+    res.sendFile(path.resolve(__dirname, "../../frontend", "build", "index.html"));
   });
 }
 
-router.get("/hello/world", function (req, res) {
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-});
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(201).json({});
+  });
+}
 
 module.exports = router;
