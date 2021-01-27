c8c3ec677fb0c2ea5b373e94c4c49ec212e66512
rob <darabandev@gmail.com>
2021-01-27 16:22:41 -0500

    Successfully pulling movies from 3rd party API into own database

--- backend/routes/api/movies.js
+++ backend/routes/api/movies.js
@@ -3,16 +3,48 @@ const asyncHandler = require("express-async-handler");
 const axios = require("axios");
 const { Movie } = require("../../db/models");
 const apiKey = require("../../apiKey");
+const { check } = require("express-validator");
 const router = express.Router();
 
+//this database did not start off seeded with every movie
+//this function determines if the app should pull movie info
+//from the local database (if it already exists), or the 3rd
+//party API.  If it pulls from the 3rd party API, it will then
+//add the pertinent info to our database to pull from there next time
+const checkLocalDbForMovie = async id => {
+  const movie = await Movie.findOne({ where: { imdbId: id } });
+  return movie !== null ? movie : false;
+};
+
 router.get(
-  "/:imdbId",
+  "/:id",
   asyncHandler(async (req, res) => {
-    const { imdbId } = req.params;
+    const { id } = req.params;
 
-    const { data } = await axios.get(`http://www.omdbapi.com/?i=${imdbId}&apikey=${apiKey}`);
+    const movieExistsInLocalDb = await checkLocalDbForMovie(id);
 
-    res.send(data);
+    if (movieExistsInLocalDb) {
+      res.send(movieExistsInLocalDb);
+    } else {
+      const { data } = await axios.get(`http://www.omdbapi.com/?i=${id}&apikey=${apiKey}`);
+      const { Title, Year, Poster, Runtime, Genre, Director, Actors, Plot, Country, imdbID, imdbRating } = data;
+
+      const newMovie = await Movie.create({
+        title: Title,
+        year: Year,
+        poster: Poster,
+        runtime: Runtime,
+        genre: Genre,
+        director: Director,
+        actors: Actors,
+        plot: Plot,
+        country: Country,
+        imdbId: imdbID,
+        imdbRating,
+      });
+
+      res.send(newMovie);
+    }
   })
 );
 
