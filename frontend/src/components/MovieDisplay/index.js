84e982d8cda19f522c9bccee4a8327e61858edec
rob <darabandev@gmail.com>
2021-01-31 16:01:45 -0500

    Styling list items and movie page displays

--- frontend/src/components/MovieDisplay/index.js
+++ frontend/src/components/MovieDisplay/index.js
@@ -4,7 +4,7 @@ import "./MovieDisplay.css";
 const MovieDisplay = ({ movie }) => {
   return (
     <div className="movie-display">
-      <h2>{movie.title}</h2>
+      <h1>{movie.title}</h1>
       <img className="movie-display-img" src={movie.poster} alt="poster" />
       <h2>{movie.year}</h2>
       <h2>{movie.runtime}</h2>
