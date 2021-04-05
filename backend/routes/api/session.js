6d9f3fe59fb8f5a51d1d5a084b37f5462d140b41
rob <darabandev@gmail.com>
2021-04-05 10:55:50 -0400

    Added requireHTTPS function to resolve csrf cookie issue

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -12,10 +12,19 @@ const validateLogin = [
   handleValidationErrors,
 ];
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https" && process.env.NODE_ENV !== "development") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
 //login existing user
 router.post(
   "/",
   validateLogin,
+  requireHTTPS,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
