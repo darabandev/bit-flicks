0b7176647cc00714842e6ce24977a7e7c2fc8da2
rob <darabandev@gmail.com>
2021-04-05 11:24:34 -0400

    Moved requireHTTPS function to app.js

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,16 @@ const { environment } = require("./config");
 const isProduction = environment === "production";
 const app = express();
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https" && process.env.NODE_ENV !== "development") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
 app.use(morgan("dev"));
+app.use(requireHTTPS);
 app.use(cookieParser());
 app.use(express.json());
 
